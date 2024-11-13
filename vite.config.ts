96fcd5a7ee48a4bc931a8b3bf9932185252d6df0
jyeoney <jiyeon2y2000@gmail.com>
2024-11-13 23:38:23 +0900

    vite.config.ts 프록시 설정 주석처리

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,42 @@
-import { defineConfig, loadEnv } from 'vite';
+// import { defineConfig, loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ command, mode }) => {
+//   const base = {
+//     plugins: [react()]
+//   };
+
+//   /**
+//    * build command 일 때는 base 설정만 사용 합니다.
+//    */
+//   if ('build' === command) {
+//     return base;
+//   }
+
+//   /**
+//    * 개발 환경에서는 proxy를 사용 합니다.
+//    */
+
+//   const env = loadEnv(mode, process.cwd(), '');
+//   return {
+//     ...base,
+//     server: {
+//       proxy: {
+//         '/api': {
+//           target: env.VITE_REST_API_KEY,
+//           changeOrigin: true,
+//           rewrite: path => path.replace(/^\/api/, '')
+//         }
+//       }
+//     }
+//   };
+// });
+
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const base = {
-    plugins: [react()]
-  };
-
-  /**
-   * build command 일 때는 base 설정만 사용 합니다.
-   */
-  if ('build' === command) {
-    return base;
-  }
-
-  /**
-   * 개발 환경에서는 proxy를 사용 합니다.
-   */
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    ...base,
-    server: {
-      proxy: {
-        '/api': {
-          target: env.ONEDAY_CLASS_API,
-          changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, '')
-        }
-      }
-    }
-  };
+export default defineConfig({
+  plugins: [react()]
 });
